03eee8846906bb3bc924762a93cfcd7c98bbe5aa
nreal000 <nreal000@gmail.com>
2015-02-14 13:52:30 -0500

    import old files: GameScreen, GameStage, Constants, WorldUtils.

---
+++ core/src/com/noreal/runnfight/utils/Constants.java
@@ -0,0 +1,23 @@
+package com.noreal.runnfight.utils;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Constants {
+
+	 	public static final int APP_WIDTH = 800;
+	    public static final int APP_HEIGHT = 480;
+
+	    public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
+
+	    public static final float GROUND_X = 0;
+	    public static final float GROUND_Y = 0;
+	    public static final float GROUND_WIDTH = 50f;
+	    public static final float GROUND_HEIGHT = 2f;
+	    public static final float GROUND_DENSITY = 0f;
+
+	    public static final float RUNNER_X = APP_WIDTH/2;
+	    public static final float RUNNER_Y = APP_HEIGHT/2;
+	    public static final float RUNNER_WIDTH = 1f;
+	    public static final float RUNNER_HEIGHT = 1f;
+	    public static float RUNNER_DENSITY = 0.5f;
+}
