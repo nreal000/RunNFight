728270ae8fd3400c3f76aa3b58f89f590500b48d
nreal000 <nreal000@gmail.com>
2015-02-14 22:12:27 -0500

    add runner

--- core/src/com/noreal/runnfight/utils/Constants.java
+++ core/src/com/noreal/runnfight/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	 	public static final int APP_WIDTH = 800;
 	    public static final int APP_HEIGHT = 480;
 
-	    public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
+	    public static final Vector2 WORLD_GRAVITY = new Vector2(0, 0);
 
 	    public static final float GROUND_X = 0;
 	    public static final float GROUND_Y = 0;
@@ -15,8 +15,8 @@ public class Constants {
 	    public static final float GROUND_HEIGHT = 2f;
 	    public static final float GROUND_DENSITY = 0f;
 
-	    public static final float RUNNER_X = APP_WIDTH/2;
-	    public static final float RUNNER_Y = APP_HEIGHT/2;
+	    public static final float RUNNER_X = 10;
+	    public static final float RUNNER_Y = 10;
 	    public static final float RUNNER_WIDTH = 1f;
 	    public static final float RUNNER_HEIGHT = 1f;
 	    public static float RUNNER_DENSITY = 0.5f;
