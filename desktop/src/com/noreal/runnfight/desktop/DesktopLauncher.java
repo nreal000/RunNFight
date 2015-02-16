4e47e2a90928e20e990e5daa13ed4a8a35c31b30
nreal000 <nreal000@gmail.com>
2015-02-16 13:04:48 -0500

    modify screen values

--- desktop/src/com/noreal/runnfight/desktop/DesktopLauncher.java
+++ desktop/src/com/noreal/runnfight/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.noreal.runnfight.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.noreal.runnfight.RunNFight;
-
+import com.noreal.runnfight.utils.Constants;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "RunNFight";
+	     config.width = Constants.APP_WIDTH;
+	     config.height = Constants.APP_HEIGHT;
 		new LwjglApplication(new RunNFight(), config);
 	}
 }
