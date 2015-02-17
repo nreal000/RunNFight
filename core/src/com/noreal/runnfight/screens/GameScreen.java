e1f7baf51f98dd96922b6124058d571bf14e498d
nreal000 <nreal000@gmail.com>
2015-02-16 22:18:56 -0500

    remove mare stage2d? stuff

--- core/src/com/noreal/runnfight/screens/GameScreen.java
+++ core/src/com/noreal/runnfight/screens/GameScreen.java
@@ -3,24 +3,17 @@ package com.noreal.runnfight.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.noreal.runnfight.stages.GameStage;
 
 public class GameScreen implements Screen {
 
-    private GameStage stage;
-
     public GameScreen() {
-        stage = new GameStage();
+        
     }
 
     @Override
     public void render(float delta) {
         //Clear the screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        //Update the stage
-        stage.draw();
-        stage.act(delta);
     }
 
 	@Override
