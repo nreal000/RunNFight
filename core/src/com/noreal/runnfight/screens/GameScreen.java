0f0b6e8d6232e3f80fe9f23d15ca2136795fc420
nreal000 <nreal000@gmail.com>
2015-02-16 22:38:42 -0500

    use constuctor as create

--- core/src/com/noreal/runnfight/screens/GameScreen.java
+++ core/src/com/noreal/runnfight/screens/GameScreen.java
@@ -3,11 +3,35 @@ package com.noreal.runnfight.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
+	
+	static final int WORLD_WIDTH = 100;
+    static final int WORLD_HEIGHT = 100;
+
+    private OrthographicCamera cam;
+    private SpriteBatch batch;
+
+    private Sprite mapSprite;
+    private float rotationSpeed;
 
     public GameScreen() {
-        
+    	 rotationSpeed = 0.5f;
+
+         mapSprite = new Sprite(new Texture(Gdx.files.internal("sc_map.png")));
+         mapSprite.setPosition(0, 0);
+         mapSprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
+
+         float w = Gdx.graphics.getWidth();
+         float h = Gdx.graphics.getHeight();
+         cam = new OrthographicCamera(30, 30 * (h / w));
+         cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+         cam.update();
+
+         batch = new SpriteBatch();
     }
 
     @Override
