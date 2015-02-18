711f865a4e245ce4c99dbbf95b750e2cfaf770c8
nreal000 <nreal000@gmail.com>
2015-02-17 20:29:56 -0500

    remove errors also changed 10 to 20

--- core/src/com/noreal/runnfight/screens/SpriteScreen.java
+++ core/src/com/noreal/runnfight/screens/SpriteScreen.java
@@ -2,7 +2,8 @@ package com.noreal.runnfight.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
@@ -58,11 +59,11 @@ public class SpriteScreen implements Screen {
 		 camera.translate(vecCamera);
 
 	        Gdx.gl.glClearColor(1, 1, 1, 1);
-	        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 	        camera.update();
 
-	        camera.translate(vecCamera.cpy().mul(-1));
+	        camera.translate(vecCamera.cpy().mul(-1f));
 
 	        float moveSensitivity = 0.9f;
 
@@ -115,31 +116,26 @@ public class SpriteScreen implements Screen {
 	        batch.end();
 	}
 
-	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void dispose() {
 		 batch.dispose();
 	     texture.dispose();
