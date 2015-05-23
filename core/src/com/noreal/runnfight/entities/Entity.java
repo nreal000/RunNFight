fac589de2fde76d89be49a092cd0e7a29a2dbbd0
nreal000 <nreal000@gmail.com>
2015-05-23 18:18:04 -0400

    change entity to Entity

---
+++ core/src/com/noreal/runnfight/entities/Entity.java
@@ -0,0 +1,8 @@
+package com.noreal.runnfight.entities;
+
+public class Entity {
+	public float x;
+	public float y;
+	public int width;
+	public int hieght;
+}
--- core/src/com/noreal/runnfight/entities/entity.java
+++
@@ -1,5 +0,0 @@
-package com.noreal.runnfight.entities;
-
-public class entity {
-
-}
