a80a1254b93745d13e19e9d72225345f8a42ed24
nreal000 <nreal000@gmail.com>
2015-05-24 12:46:23 -0400

    try  to make entity class but decided to work backwards, make entities then make entity class their similarities.

--- core/src/com/noreal/runnfight/entities/Entity.java
+++ core/src/com/noreal/runnfight/entities/Entity.java
@@ -1,8 +1,20 @@
 package com.noreal.runnfight.entities;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Entity {
 	public float x;
 	public float y;
 	public int width;
 	public int hieght;
+	
+	public Entity(float xx ,float yy){
+		this.x = xx ;
+		this.y = yy;				
+		vectormaker();
+	}
+	public Vector2 vectormaker(){
+		Vector2 thisVector = new Vector2(x, y);
+		return thisVector;
+	}
 }
