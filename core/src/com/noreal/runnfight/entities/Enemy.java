e53362b99b9789d740fdd7f87577262ae5621cdf
nreal000 <nreal000@gmail.com>
2015-05-26 12:49:10 -0400

    add direction to enemy

--- core/src/com/noreal/runnfight/entities/Enemy.java
+++ core/src/com/noreal/runnfight/entities/Enemy.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Enemy {
 	public Vector2 enemyPos;
+	public Vector2 enemyDir;
 	public Rectangle enemyRect = new Rectangle();
 	private Player player = new Player();
 	
@@ -29,7 +30,8 @@ public class Enemy {
     	
     	float anX = enemySpeed * MathUtils.cos(angle);
     	float anY = enemySpeed * MathUtils.sin(angle);
-    	enemyPos.setPosition(enemyPos + anX, enemyPos.y + anY);  // translates the x and y
+    	enemyDir = new Vector2(anX, anY);
+    	enemyPos.add(enemyDir);
 //    	System.out.println(anX + " " + anY);
 	}
 }
